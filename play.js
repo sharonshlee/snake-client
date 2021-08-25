697911a50745ad1c18e4e56895e6b78efebe44b8
Sharon Lee <sharonshlee@icloud.com>
2021-08-25 14:50:15 -0400

    refactor using modules

--- play.js
+++ play.js
@@ -1,26 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.223.133", // IP address here,
-    port: 50542, // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  conn.on("connect", (data) => {
-    //handle the connect event
-    conn.write("Hello from client!");
-  });
-
-  return conn;
-};
+const client = require("./client");
 
 console.log("Connecting ...");
-connect();
+client.connect();
