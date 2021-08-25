00aec85f793146da855307d6747b871a00c5c96f
Sharon Lee <sharonshlee@icloud.com>
2021-08-25 15:58:12 -0400

    stdin setup, ctr+c to terminate game

--- play.js
+++ play.js
@@ -2,3 +2,24 @@ const client = require("./client");
 
 console.log("Connecting ...");
 client.connect();
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+setupInput();
